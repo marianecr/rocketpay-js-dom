ae118dcefc636604d9fe760bd329dbd16043ad9d
Mariane Rodrigues <marianectrodrigues@gmail.com>
2022-10-22 19:32:44 -0300

    add 3 more credit cards and change logos

--- src/main.js
+++ src/main.js
@@ -9,7 +9,9 @@ function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
-    // Adicionar american express depois
+    maestro: ["#0007B1", "#B20000"],
+    jcb: ["#91CE0F", "#E70707"],
+    amex: ["#E3E6F2", "#2AA2F9"],
     default: ["black", "gray"],
   }
 
@@ -55,6 +57,21 @@ const cardNumberPattern = {
       mask: "0000 0000 0000 0000",
       regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
       cardtype: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:5[0678]\d{0,2}|6304|67\d{0,2})\d{0,12}/,
+      cardtype: "maestro",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:35\d{0,2})\d{0,12}/,
+      cardtype: "jcb",
+    },
+    {
+      mask: "0000 000000 00000",
+      regex: /^3[47]\d{0,13}/,
+      cardtype: "amex",
     },
     {
       mask: "0000 0000 0000 0000",
